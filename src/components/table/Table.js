4e63996c1d4e48c2229741312a2d13bdca745ad3
ViolettaParamonova <violettapar1995@gmail.com>
2022-04-14 13:43:37 +0300

    table-logic save, stop

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,14 +1,16 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import { createTable } from './table.template'
 import { resizeHandler } from './table.resize'
-import { shouldResize } from './table.functions'
+import { isCell, shouldResize, matrix, nextSelector } from './table.functions'
+import { TableSelection } from './TableSelection'
+import { $ } from "@core/dom"
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
 
     constructor ($root) {
         super ($root, {
-            listeners: ['mousedown'] 
+            listeners: ['mousedown', 'keydown'] 
         })
     }
 
@@ -16,9 +18,39 @@ export class Table extends ExcelComponent {
         return createTable ()
     }
 
+    prepare () {
+        this.selection = new TableSelection()
+    }
+
+    init () {
+        super.init()
+        const $cell = this.$root.find('[data-id="0:0"]')
+        this.selection.select($cell)
+    }
+
     onMousedown (event) {
         if (shouldResize (event) ) {
             resizeHandler (this.$root, event)
+        } else if (isCell(event)) {
+            const $target = $(event.target)
+            if (event.shiftKey) {
+                const $cells = matrix ($target, this.selection.current)
+                .map(id => this.$root.find(`[data-id="${id}"]`))
+                this.selection.selectGroup($cells)
+            } else {
+                this.selection.select($target)
+            }
+        }
+    }
+
+    onKeydown (event) {
+        const keys = ['Enter', 'Tab', 'ArrowRight', 'ArrowLeft', 'ArrowDown', 'ArrowUp']
+        const {key} = event
+        if (keys.includes(event.key) && !event.shiftKey) {
+            event.preventDefault ()
+            const id =  this.selection.current.id(true)
+            const $next =this.$root.find(nextSelector(key, id))
+            this.selection.select($next)
         }
     }
 }
